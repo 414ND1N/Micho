9002b324033f2653f8dc89c65b31b0b9c1b30fa8
414ND1N <414nd1n@gmail.com>
2024-06-09 01:03:22 -0600

    Comando avatar, implementacion banner

--- src/commands/slashCommands/Admin/avatar.js
+++ src/commands/slashCommands/Admin/avatar.js
@@ -1,76 +1,98 @@
-const {SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits} = require('discord.js')
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js')
+const { Routes, DataResolver } = require('discord.js')
 
 module.exports = {
+    cooldown: 20,
     CMD: new SlashCommandBuilder()
         .setName("avatar")
-        .setNameLocalizations({  "en-US": "avatar" })
+        .setNameLocalizations({ "en-US": "avatar" })
         .setDescription(`Actualizar el avatar de ${process.env.BOT_NAME}`)
         .setDescriptionLocalizations({
             "en-US": `Update ${process.env.BOT_NAME}'s avatar`
         })
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
-        .addSubcommand(subcommand =>
-            subcommand.setName("enlace")
-                .setNameLocalizations({ "en-US": "link" })
-                .setDescription('Establece un avatar estático desde un enlace')
-                .setDescriptionLocalizations({ "en-US": "Set a static avatar from an url" })
-                .addStringOption(option =>
-                    option.setName('url')
-                        .setDescription('Enlace de la imagen a setear como avatar')
-                        .setDescriptionLocalizations({ "en-US": "Image link to set as avatar" })
-                        .setRequired(true)
+        .addSubcommandGroup(subcommandGroup =>
+            subcommandGroup
+                .setName('perfil')
+                .setNameLocalizations({ "en-US": "profile" })
+                .setDescription('Establece la imagen de perfil del bot')
+                .setDescriptionLocalizations({ "en-US": "Set the bot profile image" })
+                .addSubcommand(subcommand =>
+                    subcommand.setName("enlace")
+                        .setNameLocalizations({ "en-US": "link" })
+                        .setDescription('Establece un avatar estático desde un enlace')
+                        .setDescriptionLocalizations({ "en-US": "Set a static avatar from an url" })
+                        .addStringOption(option =>
+                            option.setName('url')
+                                .setDescription('Enlace de la imagen a setear como avatar')
+                                .setDescriptionLocalizations({ "en-US": "Image link to set as avatar" })
+                                .setRequired(true)
+                        )
+                )
+                .addSubcommand(subcommand =>
+                    subcommand.setName("archivo")
+                        .setNameLocalizations({ "en-US": "dynamic" })
+                        .setDescription('Establece un avatar dinámico desde un archivo')
+                        .setDescriptionLocalizations({ "en-US": "Set a dinamic avatar from an image file" })
+                        .addAttachmentOption(option =>
+                            option.setName('archivo')
+                                .setDescription('Archivo a setear como avatar')
+                                .setDescriptionLocalizations({ "en-US": "File to set as avatar" })
+                                .setRequired(true)
+                        )
                 )
         )
-        .addSubcommand(subcommand =>
-            subcommand.setName("archivo")
-                .setNameLocalizations({ "en-US": "dynamic" })
-                .setDescription('Establece un avatar dinámico desde un archivo')
-                .setDescriptionLocalizations({ "en-US": "Set a dinamic avatar from an image file" })
-                .addAttachmentOption(option =>
-                    option.setName('archivo')
-                        .setDescription('Archivo a setear como avatar')
-                        .setDescriptionLocalizations({ "en-US": "File to set as avatar" })
-                        .setRequired(true)
+        .addSubcommandGroup(subcommandGroup =>
+            subcommandGroup
+                .setName('fondo')
+                .setNameLocalizations({ "en-US": "banner" })
+                .setDescription('Establece la imagen de fondo del bot')
+                .setDescriptionLocalizations({ "en-US": "Set the bot banner" })
+                .addSubcommand(subcommand =>
+                    subcommand.setName("enlace")
+                        .setNameLocalizations({ "en-US": "link" })
+                        .setDescription('Establece un avatar estático desde un enlace')
+                        .setDescriptionLocalizations({ "en-US": "Set a static avatar from an url" })
+                        .addStringOption(option =>
+                            option.setName('url')
+                                .setDescription('Enlace de la imagen a setear como avatar')
+                                .setDescriptionLocalizations({ "en-US": "Image link to set as avatar" })
+                                .setRequired(true)
+                        )
+                )
+                .addSubcommand(subcommand =>
+                    subcommand.setName("archivo")
+                        .setNameLocalizations({ "en-US": "dynamic" })
+                        .setDescription('Establece un avatar dinámico desde un archivo')
+                        .setDescriptionLocalizations({ "en-US": "Set a dinamic avatar from an image file" })
+                        .addAttachmentOption(option =>
+                            option.setName('archivo')
+                                .setDescription('Archivo a setear como avatar')
+                                .setDescriptionLocalizations({ "en-US": "File to set as avatar" })
+                                .setRequired(true)
+                        )
                 )
         )
-        ,
-    
-    async execute(client, interaction){
-       
+    ,
+
+    async execute(client, interaction) {
+
         try {
 
             interaction.deferReply({ ephemeral: true })
 
+            const VALID_FORMATS = ['image/jpeg', 'image/png', 'image/webp', 'image/gif']
+            let URL = ''
+
             switch (interaction.options.getSubcommand()) {
                 case 'enlace':
-                    const url = interaction.options.getString('url')
-                    client.user.setAvatar(url).catch(async err => {
-                        console.error(err)
-                        await interaction.editReply({
-                            embeds: [
-                                new EmbedBuilder()
-                                    .setTitle(`Error al intentar cambiar el avatar`)
-                                    .setColor(process.env.COLOR_ERROR)
-                            ],
-                            ephemeral: true
-                        })
-                        return
-                    })
-
-                    return interaction.editReply({
-                        embeds: [
-                            new EmbedBuilder()
-                                .setTitle(`Cambio de avatar de \`${process.env.BOT_NAME}\``)
-                                .setColor(process.env.COLOR)
-                                .setImage(url)
-                        ],
-                        ephemeral: true
-                    })
+                    URL = interaction.options.getString('url')
+                    break
                 case 'archivo':
                     const { options } = interaction
                     const file = options.getAttachment("archivo")
-                    
-                    if (!file.contentType.startsWith('image')) {
+
+                    if (!VALID_FORMATS.includes(file.contentType)) {
                         await interaction.editReply({
                             embeds: [
                                 new EmbedBuilder()
@@ -82,10 +104,15 @@ module.exports = {
                         })
                         return
                     }
+                    URL = file.url
+                    break
+            }
 
-                    await client.user.setAvatar(file.url).catch(async err => {
+            switch (interaction.options.getSubcommandGroup()) {
+                case 'perfil':
+                    await client.user.setAvatar(URL).catch(async err => {
                         console.error(err)
-                        await interaction.editReply({
+                        return await interaction.editReply({
                             embeds: [
                                 new EmbedBuilder()
                                     .setTitle(`Error al intentar cambiar el avatar`)
@@ -93,26 +120,70 @@ module.exports = {
                             ],
                             ephemeral: true
                         })
-                        return 
                     })
 
-                    await interaction.editReply({
+                    return await interaction.editReply({
                         embeds: [
                             new EmbedBuilder()
                                 .setTitle(`Cambio de avatar de \`${process.env.BOT_NAME}\``)
                                 .setColor(process.env.COLOR)
-                                .setImage(file.url)
+                                .setImage(URL)
+                        ],
+                        ephemeral: true
+                    })
+                case 'fondo':
+
+                    await client.rest.patch("/users/@me", {
+                        body: {
+                            banner: "data:image/gif;base64," + Buffer.from(
+                                await (await fetch(URL)).arrayBuffer()).toString('base64')
+                        }
+                    }).catch(async err => {
+                        console.error(err)
+                        await interaction.editReply({
+                            embeds: [
+                                new EmbedBuilder()
+                                    .setTitle(`Error al intentar cambiar el banner`)
+                                    .setColor(process.env.COLOR_ERROR)
+                            ],
+                            ephemeral: true
+                        })
+                        return
+                    })
+
+                    // await client.rest.patch(Routes.user(), {
+                    //     body: { banner: await DataResolver.resolveImage(URL) }
+                    // }).catch(async err => {
+                    //     console.error(err)
+                    //     await interaction.editReply({
+                    //         embeds: [
+                    //             new EmbedBuilder()
+                    //                 .setTitle(`Error al intentar cambiar el banner`)
+                    //                 .setColor(process.env.COLOR_ERROR)
+                    //         ],
+                    //         ephemeral: true
+                    //     })
+                    //     return
+                    // })
+
+                    return interaction.editReply({
+                        embeds: [
+                            new EmbedBuilder()
+                                .setTitle(`Cambio de banner de \`${process.env.BOT_NAME}\``)
+                                .setColor(process.env.COLOR)
+                                .setImage(URL)
                         ],
                         ephemeral: true
                     })
             }
+
         } catch (error) {
             console.error(error)
-            return interaction.reply({ 
-                content: `Ocurrió un error al intentar cambiar el avatar de ${process.env.BOT_NAME}`, 
+            return interaction.reply({
+                content: `Ocurrió un error al intentar cambiar el avatar de ${process.env.BOT_NAME}`,
                 ephemeral: true
             })
         }
-        
+
     }
 }  
